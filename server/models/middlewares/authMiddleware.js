c977071777d55ae243d3d276cc99b9e4080e4087
Kishan Ahir <kk57161@gmail.com>
2026-02-20 17:33:39 +0530

    Revert "temporary commit before revert"
    This reverts commit 463888c683d9023219fb6b5563b32a7aba7f45fd.

--- server/models/middlewares/authMiddleware.js
+++ server/models/middlewares/authMiddleware.js
@@ -7,11 +7,3 @@ export const checkRole = (allowedRoles) => {
     next();
   };
 };
-
-export const verifyToken = (req, res, next) => {
-  const token = req.headers["authorization"];
-  if (!token) {
-    return res.status(401).json({ message: "Access denied" });
-  }
-  next();
-};
